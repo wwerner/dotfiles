a3aed3c0eca19a4c9625e98a3a595f6b4e88960d
Wolfgang Werner <mail@wolfgang-werner.net>
2019-06-08 21:38:23 +0200

    Clean up brew deps

--- bin/install-brew-deps.sh
+++ bin/install-brew-deps.sh
@@ -1,58 +1,28 @@
 #!/usr/bin/env bash
 
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew tap caskroom/fonts
 brew cask install font-hack-nerd-font
 # brew install jenv
-brew install cask
-brew install homebrew-cask
-brew tap caskroom/versions
-brew tap homebrew/cask
-brew tap homebrew/cask-versions
-brew cask install java8
-brew install maven
 brew install watch
 brew install htop
 brew install jq
 brew install git-flow-avh
-brew install keybase
-brew cask install mounty
 brew install heroku
 brew install icdiff
-brew cask install real-vnc
-brew install httpie
-brew install ghostscript
 brew install coreutils
 brew cask install vagrant
 brew cask install virtualbox-extension-pack
 brew install gpg
-brew install skype
-brew install mat
-brew install nuget
 brew install nmap
-brew install flyway
-brew install gitk
-brew install gitter
 brew install bat
 brew install wget
 brew install unrar
-brew install hub
-brew install shellcheck
 brew install mailcatcher
-brew cask install android-platform-tools
 brew cask install the-unarchiver
 brew install bat
 brew install prettyping
 brew install fzf
 brew install tldr
-brew install asciinema
-brew install cloc
-brew install gource
-brew install ffmpeg
-brew install pandoc
-brew cask install macpass
 brew cask install cyberduck
-brew cask install minikube
 brew install golang
 brew cask install hammerspoon
 brew cask install iterm2
