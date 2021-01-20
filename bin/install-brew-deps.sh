10f78893ee71c64f1ba3b1961185f4975a40839b
Wolfgang Werner <wolfgang.werner@gmail.com>
2021-01-20 09:58:58 +0100

    Update tools installed w/ brew

--- bin/install-brew-deps.sh
+++ bin/install-brew-deps.sh
@@ -1,32 +1,42 @@
 #!/usr/bin/env bash
 
-brew cask install font-hack-nerd-font
-brew install node
-brew install watch
-brew install htop
-brew install jq
-brew install git-flow-avh
-brew install heroku
-brew install icdiff
-brew install coreutils
-brew cask install vagrant
-brew cask install virtualbox-extension-pack
-brew install gpg
-brew install nmap
-brew install bat
-brew install wget
-brew install unrar
-brew cask install the-unarchiver
-brew install bat
-brew install prettyping
-brew install fzf
-brew install tldr
-brew install golang
-brew cask install hammerspoon
-brew cask install iterm2
-brew cask install caskroom/versions/visual-studio-code-insiders
-brew install thefuck
+brew cask install kitty
+brew cask install cyberduck
 brew cask install gimp
+brew cask install macpass
+brew cask install visual-studio-code-insiders
+brew cask install hammerspoon
+brew cask install font-hack-nerd-font
+brew cask install the-unarchiver
+
+brew install bat
+brew install coreutils
+brew install curl
+brew install curl-openssl
+brew install exiftool
+brew install ffmpeg
+brew install flac
+brew install fzf
+brew install ghostscript
+brew install git-flow-avh
+brew install gpg
+brew install graphviz
+brew install htop
+brew install httpie
+brew install icdiff
+brew install imagemagick
+brew install jq
+brew install k9s
+brew install kafkacat
+brew install macpass
+brew install nmap
+brew install pandoc
+brew install prettyping
+brew install shellcheck
+brew install tldr
+brew install unrar
+brew install watch
+brew install wget
 brew install z
 
 
